693e63dcd325fe03e51e9fe397fe246ac8da6b89
Pedro Neves <ptersnow@gmail.com>
2019-09-23 10:35:22 -0400

    Ajustando o postgresql

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -14,13 +21,6 @@ return [
     */
 
     'default' => env('DB_CONNECTION', 'pgsql'),
-
-    $url = parse_url(getenv("DATABASE_URL"));
-
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
 
     /*
     |--------------------------------------------------------------------------
